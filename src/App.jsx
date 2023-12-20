7c2e325d2dc1f4ec7343e0793f19e6b4f7dfb5d9
TanvirCoU <ahmeadtanvir@stud.cou.ac.bd>
2023-12-21 02:14:50 +0600

    route added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import {
 } from "react-router-dom";
 import Home from "./components/Home/Home/Home";
 import Chat from "./components/Chat/Chat/Chat";
+import PublicRoute from "./components/Route/PublicRoute";
+import PrivateRoute from "./components/Route/PrivateRoute";
 
 function App() {
 
   return (
     <div className="">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/" element={<PublicRoute> <Home /> </PublicRoute>} />
+        <Route path="/chat" element={<PrivateRoute> <Chat /> </PrivateRoute>} />
       </Routes>
     </div>
   )
--- src/components/context/UserProvider.jsx
+++ src/components/context/UserProvider.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
@@ -9,16 +8,11 @@ const UserProvider = ({ children }) => {
     const [selectedChat, setSelectedChat] = useState();
     const [chats, setChats] = useState([]);
     const [notifications, setNotifications] = useState([]);
-    const navigate = useNavigate();
 
     useEffect(() => {
         const userInfo = JSON.parse(localStorage.getItem("userData"));
         setUser(userInfo);
-
-        if(!userInfo) {
-            navigate("/");
-        }
-    }, [navigate])
+    }, [])
 
     return (
     <UserContext.Provider value={{user, setUser, selectedChat, setSelectedChat, chats, setChats, notifications, setNotifications}}>
